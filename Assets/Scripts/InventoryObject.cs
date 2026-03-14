0f31361d906ff1e45241c372d483d963d3ddd5ac
Nick Pasta <84762796+nickpastoria@users.noreply.github.com>
2026-03-14 11:41:54 -0700

    UI Overhaul

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -12,29 +12,45 @@ public class InventoryObject : MonoBehaviour
 
     private Image buttonImage;
 
+    private GameObject ImageButton;
+
     public void Make(ItemBrowser.Type type, int u, string n, SpriteLibrary spl)
     {
+        Image SpriteImage;
         name = n;
         UID = u;
         SPL = spl;
         textLabel = GetComponentInChildren<TMP_Text>();
         textLabel.text = name;
+        ImageButton = FindChildWithTag(this.gameObject, "Toolbox Image");
+        SpriteImage = ImageButton.GetComponent<Image>();
+
         if (type == ItemBrowser.Type.Spawn)
         {
             if(SPL.spawnImgExists(UID))
             {
-                buttonImage = GetComponentInChildren<Image>();
-                buttonImage.sprite = SPL.findSpawnByID(UID);
+                SpriteImage.sprite = SPL.findSpawnByID(UID);
             }
         }
         if(type == ItemBrowser.Type.Tile)
         {
             if(SPL.tileImgExists(UID))
             {
-                buttonImage = GetComponentInChildren<Image>();
-                buttonImage.sprite = SPL.findTileByID(UID);
+                SpriteImage.sprite = SPL.findTileByID(UID);
             }
         }
+    }
+    GameObject FindChildWithTag(GameObject parent, string tag) {
+        GameObject child = null;
+
+        foreach(Transform transform in parent.transform) {
+            if(transform.CompareTag(tag)) {
+                child = transform.gameObject;
+                break;
+            }
+        }
+
+    return child;
     }
 
 }
